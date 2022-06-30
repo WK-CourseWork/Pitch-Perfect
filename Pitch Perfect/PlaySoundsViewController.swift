6bafe17eee117ae7ca47f346d4eb104a7e759057
waylonkumpe <waylon@kumpes.com>
2022-06-30 14:38:04 -0500

    Initial commit

---
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -0,0 +1,58 @@
+//
+//  PlaySoundsViewController.swift
+//  Pitch Perfect
+//
+//  Created by Waylon Kumpe on 6/29/22.
+//
+
+import UIKit
+import AVFoundation
+class PlaySoundsViewController: UIViewController {
+    @IBOutlet weak var slow: UIButton!
+    @IBOutlet weak var chipmunk: UIButton!
+    @IBOutlet weak var fast: UIButton!
+    @IBOutlet weak var vader: UIButton!
+    @IBOutlet weak var echo: UIButton!
+    @IBOutlet weak var reverb: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
+    var recordedAudioURL:URL!
+    var audioFile:AVAudioFile!
+    var audioEngine:AVAudioEngine!
+    var audioPlayerNode: AVAudioPlayerNode!
+    var stopTimer: Timer!
+
+    enum ButtonType: Int {
+        case slow = 0, fast = 1, chipmunk = 2, vader = 3, echo = 4, reverb = 5
+    }
+    @IBAction func playSoundForButton(_ sender: UIButton) {
+        switch(ButtonType(rawValue: sender.tag)!) {
+           case .slow:
+               playSound(rate: 0.5)
+           case .fast:
+               playSound(rate: 1.5)
+           case .chipmunk:
+               playSound(pitch: 1000)
+           case .vader:
+               playSound(pitch: -1000)
+           case .echo:
+               playSound(echo: true)
+           case .reverb:
+               playSound(reverb: true)
+           }
+
+           configureUI(.playing)
+    }
+    @IBAction func stopButtonPressed(_ sender: AnyObject) {
+        stopAudio()
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupAudio()
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        configureUI(.notPlaying)
+    }
+
+}
---
+++ Pitch Perfect/RecordSoundViewController.swift
@@ -0,0 +1,74 @@
+//
+//  RecordSoundViewController.swift
+//  Pitch Perfect
+//
+//  Created by Waylon Kumpe on 6/27/22.
+//
+
+import UIKit
+import AVFoundation
+
+class recordSoundViewController: UIViewController, AVAudioRecorderDelegate {
+    var audioRecorder = AVAudioRecorder()
+    
+    @IBOutlet weak var recordinglabel: UILabel!
+    @IBOutlet weak var recordingButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        stopRecordingButton.isEnabled = false
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    @IBAction func recordaudieo(_ sender: Any) {
+        #error("The next 3 lines are repeditive")
+        recordinglabel.text = "Recording in progress"
+        stopRecordingButton.isEnabled = true
+        recordingButton.isEnabled = false
+        let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory,.userDomainMask, true)[0] as String
+        let recordingName = "recordedVoice.wav"
+        let pathArray = [dirPath, recordingName]
+        let filePath = URL(string: pathArray.joined(separator: "/"))
+        print(filePath!)
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(AVAudioSession.Category.playAndRecord, mode: AVAudioSession.Mode.default, options: AVAudioSession.CategoryOptions.defaultToSpeaker)
+
+        try! audioRecorder = AVAudioRecorder(url: filePath!, settings: [:])
+        audioRecorder.delegate = self
+        audioRecorder.isMeteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
+    }
+    
+    @IBAction func stoprecording(_ sender: Any) {
+        #error("the next 3 lines are repeditive")
+        recordingButton.isEnabled = true
+        stopRecordingButton.isEnabled = false
+        recordinglabel.text = "Tap to Record"
+        changeText(words: "tap to record")
+        audioRecorder.stop()
+           let audioSession = AVAudioSession.sharedInstance()
+           try! audioSession.setActive(false)
+    }
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        if flag {
+            performSegue(withIdentifier: "stopRecording", sender: audioRecorder.url)
+        } else {
+            print("Recording was not successful")
+            
+        }
+        
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "stopRecording" {
+            let playSoundsVC = segue.destination as! PlaySoundsViewController
+            let recordedAudioURL = sender as! URL
+            playSoundsVC.recordedAudioURL = recordedAudioURL
+        }
+    }
+    
+}
+
